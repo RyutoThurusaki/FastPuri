a44e6558d2e76c27dd812f35541cc67a5c716a79
masumi uozumi <41750783+MasumiUozumi@users.noreply.github.com>
2022-09-10 08:20:19 +0900

    アウトライン機能の追加
    その他、将来必要なボタンと素材類の追加。
    前回コミットのりファクタによって不要になった処理を削除。

--- FastPuri/MainWindow.xaml.cs
+++ FastPuri/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Ink;
 
 namespace FastPuri
 {
@@ -26,11 +27,12 @@ namespace FastPuri
         string[] Filepaths = {"Dammy"};
 
         int FileOrder = 0;
-        int FileOrderCache = 0;
+        int OutlineSize = 5;
 
         bool isPainting = false;
+        bool isOutline = false;
 
-        //DispatcherTimer updatetimer = null;
+        DispatcherTimer updatetimer = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -41,16 +43,17 @@ namespace FastPuri
         public void AddUpdate()
         {
             updatetimer = new DispatcherTimer(DispatcherPriority.Background);
-            updatetimer.Interval = new TimeSpan(0, 0, 1);
+            updatetimer.Interval = new TimeSpan(0,0,0);
             updatetimer.Tick += (e, s) => { Debug_Update(); };
 
             updatetimer.Start();
         }
+
         public void Debug_Update()
         {
-            LabelInfomation_1.Content = MainCanvas.Height;
-        }
-        */
+
+        }*/
+
         private void ClicktoOpen(object sender, RoutedEventArgs e)
         {
             OpenFileDialog filedialog = new OpenFileDialog();
@@ -62,47 +65,48 @@ namespace FastPuri
                 Open_Savedialog(filedialog, filedialog.FileName, true,true);
             }
         }
+
         private void Button_Prev_Click(object sender, RoutedEventArgs e)
         {
             if (FileOrder > 0)
             {
-                FileOrderCache = FileOrder;
                 FileOrder--;
                 Open_Savedialog(null, Filepaths[FileOrder], false,true);
             }
             else
             {
-                FileOrderCache = FileOrder;
                 FileOrder = Filepaths.Length-1;
                 Open_Savedialog(null, Filepaths[FileOrder], false, true);
             }
         }
+
         private void Button_Next_Click(object sender, RoutedEventArgs e)
         {
             if (FileOrder < Filepaths.Length-1)
             {
-                FileOrderCache = FileOrder;
                 FileOrder++;
                 Open_Savedialog(null, Filepaths[FileOrder], false, true);
             }
             else
             {
-                FileOrderCache = FileOrder;
                 FileOrder = 0;
                 Open_Savedialog(null, Filepaths[FileOrder],false, true);
             }
         }
+
         private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             //Set paintingstatus.
             isPainting = true;
             LabelInfomation.Foreground = Brushes.Yellow;
         }
+
         private void Open_Savedialog(FileDialog filedialog,string filename,bool isloadarray,bool isload)
         {
-            //Array load cansel.
+            //Array load cancel.
             bool load = isload;
 
+            //Ask if want to save image.
             if (isPainting)
             {
                 MessageBoxResult result = MessageBox.Show("プリクラを保存しますか？", "SaveDialog", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
@@ -110,11 +114,8 @@ namespace FastPuri
                 if (result == MessageBoxResult.Yes)
                 {
                     load = true;
-                    isPainting = false;
-                    LabelInfomation.Foreground = Brushes.White;
 
-                    //image saveing...under maintenance...
-
+                    Save_Image();
                 }
                 else if (result == MessageBoxResult.No)
                 {
@@ -122,16 +123,17 @@ namespace FastPuri
                     isPainting = false;
                     LabelInfomation.Foreground = Brushes.White;
 
+                    OutlineCanvas.Strokes.Clear();
                     MainCanvas.Strokes.Clear();
                 }
                 else if (result == MessageBoxResult.Cancel)
                 {
                     load = false;
-                    FileOrder = FileOrderCache;
                     //do not
                 }
             }
 
+            //Reload file pathes in array.
             if (isloadarray == true && load == true)
             {
                 // Save directory path.
@@ -143,10 +145,9 @@ namespace FastPuri
                 Filepaths = System.IO.Directory.GetFiles(Defaultfilepath, extension, SearchOption.TopDirectoryOnly);
 
                 FileOrder = Array.IndexOf(Filepaths, filedialog.FileName);
-
-                FileOrderCache = FileOrder;
             }
 
+            //Image opening to canvas.
             if (load)
             {
                 //Null check.
@@ -175,8 +176,72 @@ namespace FastPuri
                 }
                 else
                 {
-                    //MainImage.Source = null;
+                    MainImage.Source = null;
                 }
+            }
+        }
+
+        private void Save_Image()
+        {
+
+            OutlineCanvas.Strokes.Clear();
+            MainCanvas.Strokes.Clear();
+
+            isPainting = false;
+            LabelInfomation.Foreground = Brushes.White;
+        }
+
+        private void OutlineEnable_Click(object sender, RoutedEventArgs e)
+        {
+            isOutline = !isOutline;
+            MainCanvas_PreviewMouseUp(this,null);
+
+            if (!isOutline)
+            {
+                OutlineCanvas.Strokes.Clear();
+            }
+        }
+
+        private void Mode_Eraser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mode_Pen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void MainCanvas_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!object.ReferenceEquals(sender, this))
+            {
+                //This function summon is mouse event other possibility.
+                //if Mouse event only.
+                LabelInfomation.Foreground = Brushes.Yellow;
+                isPainting = true;
+            }
+
+            await Task.Delay(1);
+
+            if (isOutline)
+            {
+                DrawingAttributes inkda = new DrawingAttributes();
+
+                inkda.Width = MainCanvas.DefaultDrawingAttributes.Width + OutlineSize;
+                inkda.Height = MainCanvas.DefaultDrawingAttributes.Height + OutlineSize;
+
+                inkda.Color = Colors.White;
+
+                OutlineCanvas.Strokes = MainCanvas.Strokes.Clone();
+
+                for (int i = 0; i < MainCanvas.Strokes.Count; i++)
+                {
+                    this.OutlineCanvas.Strokes[i].DrawingAttributes = inkda;
+                }
+
+                int hoge = MainCanvas.Strokes.Count;
+                //MessageBox.Show(hoge.ToString(), "StrokeCounts");
             }
         }
     }
